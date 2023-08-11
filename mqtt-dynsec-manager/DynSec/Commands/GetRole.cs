6165c968c4c0aa92981ab0dfc0579180a2e76dc7
Rafael Gustavo da Cunha Pereira Pinto <rafaelgcpp@gmail.com>
2023-08-11 15:30:29 -0300

    I'm fixing a hole...

--- mqtt-dynsec-manager/DynSec/Commands/GetRole.cs
+++ mqtt-dynsec-manager/DynSec/Commands/GetRole.cs
@@ -4,7 +4,7 @@ namespace mqtt_dynsec_manager.DynSec.Commands
 {
     public sealed class GetRole : AbstractCommand
     {
-        public GetRole(string rolename) : base("getClient") { _rolename = rolename; }
+        public GetRole(string rolename) : base("getRole") { _rolename = rolename; }
         private readonly string _rolename;
         public string RoleName { get { return _rolename; } }
     }
--- mqtt-dynsec-manager/DynSec/Responses/Abstract/AbstractResponseData.cs
+++ mqtt-dynsec-manager/DynSec/Responses/Abstract/AbstractResponseData.cs
@@ -1,5 +1,13 @@
-﻿namespace mqtt_dynsec_manager.DynSec.Responses.Abstract
+﻿using System.Text.Json.Serialization;
+
+namespace mqtt_dynsec_manager.DynSec.Responses.Abstract
 {
+    [JsonDerivedType(typeof(AnonymousGroupInfoData))]
+    [JsonDerivedType(typeof(ClientInfoData))]
+    [JsonDerivedType(typeof(ClientListData))]
+    [JsonDerivedType(typeof(GroupInfoData))]
+    [JsonDerivedType(typeof(GroupListData))]
+    [JsonDerivedType(typeof(RoleListData))]
     public abstract class AbstractResponseData
     {
     }
